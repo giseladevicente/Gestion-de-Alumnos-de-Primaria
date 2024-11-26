e491e86d8956a752d5fd710808d540ea8177a1cd
giseladevicente <devicentegisela@gmail.com>
2024-11-26 18:31:00 -0300

    modificaciones en lista_comunicados.html

--- app.py
+++ app.py
@@ -220,138 +220,7 @@ def lista_tareas():
     return render_template('lista_tareas.html', tareas=tareas, fecha_actual=fecha_actual)
 
 
-# COMUNICADOS
-@app.route('/crear_comunicado', methods=['GET', 'POST'])
-def crear_comunicado():
-    if 'user_id' not in session or session.get('role') != 'docente':
-        return redirect(url_for('index'))  
 
-    mensaje = None  # variable que almacena mensaje de éxito
-
-    if request.method == 'POST':  
-        contenido = request.form['contenido']  
-
-        cur = mysql.connection.cursor()
-        cur.execute(
-            "INSERT INTO comunicados (docente_id, tipo_comunicado, contenido) VALUES (%s, %s, %s)",
-            (session['user_id'], 'general', contenido)
-        )
-        mysql.connection.commit()  
-        cur.close()
-
-        mensaje = "Comunicado general creado exitosamente."
-        
-        return redirect(url_for('lista_comunicados', mensaje=mensaje))
-
-    return render_template('crear_comunicado.html', mensaje=mensaje)  
-
-
-@app.route('/comunicados')
-def lista_comunicados():
-    if 'user_id' not in session:
-        return redirect(url_for('index'))
-
-    mensaje = request.args.get('mensaje')
-    user_id = session['user_id']
-    role = session.get('role')
-
-    cur = mysql.connection.cursor()
-
-    if role == 'docente':
-        cur.execute("""
-SELECT c.id, c.contenido, c.fecha_envio, c.tipo_comunicado,
-            (SELECT JSON_ARRAYAGG(JSON_OBJECT('respuesta', r.respuesta, 'remitente', u.nombre_completo, 'fecha', r.fecha_respuesta))
-            FROM respuestas_comunicados r
-            JOIN usuarios u ON r.remitente_id = u.id
-            WHERE r.comunicado_id = c.id) AS respuestas,
-            (SELECT GROUP_CONCAT(u.nombre_completo SEPARATOR ', ') 
-            FROM comunicados_destinatarios cd 
-            JOIN usuarios u ON cd.usuario_id = u.id 
-            WHERE cd.comunicado_id = c.id) AS destinatarios
-        FROM comunicados c
-        LEFT JOIN comunicados_destinatarios cd ON c.id = cd.comunicado_id
-        WHERE c.docente_id = %s OR c.tipo_comunicado = 'general' OR cd.usuario_id = %s
-        GROUP BY c.id
-        ORDER BY c.fecha_envio DESC
-        """, (user_id, user_id))
-    else:  # Rol alumno u otro
-        cur.execute("""       
-        SELECT c.id, c.contenido, c.fecha_envio, c.tipo_comunicado,           
-            (SELECT JSON_ARRAYAGG(JSON_OBJECT('respuesta', r.respuesta, 'remitente', u.nombre_completo, 'fecha', r.fecha_respuesta))
-             FROM respuestas_comunicados r
-             JOIN usuarios u ON r.remitente_id = u.id
-             WHERE r.comunicado_id = c.id) AS respuestas,                   
-            (SELECT GROUP_CONCAT(u.nombre_completo SEPARATOR ', ') 
-             FROM comunicados_destinatarios cd 
-             JOIN usuarios u ON cd.usuario_id = u.id 
-             WHERE cd.comunicado_id = c.id) AS destinatarios                             
-        FROM comunicados c
-        LEFT JOIN comunicados_destinatarios cd ON c.id = cd.comunicado_id
-        WHERE c.tipo_comunicado = 'general' OR (c.tipo_comunicado = 'personalizado' AND cd.usuario_id = %s)
-        GROUP BY c.id
-        ORDER BY c.fecha_envio DESC
-        """, (user_id,))
-
-    comunicados = cur.fetchall()
-    cur.close()
-
-    comunicados_format = [
-        (row[0], row[1], row[2], row[3], eval(row[4]) if row[4] else None, row[5])  
-        for row in comunicados
-    ]
-
-    print(comunicados)
-
-
-    return render_template('lista_comunicados.html', comunicados=comunicados_format, mensaje=mensaje)
-
-
-# COMUNICADOS PERSONALIZADOS
-@app.route('/crear_comunicado_personalizado', methods=['GET', 'POST'])
-def crear_comunicado_personalizado():
-    if 'user_id' not in session or session.get('role') != 'docente':
-        return redirect(url_for('index'))  
-     
-    if request.method == 'POST':  
-        destinatario = request.form['destinatario']
-        contenido = request.form['contenido']
-        tipo_comunicado = request.form['personalizado']
-
-        cur = mysql.connection.cursor()
-        cur.execute(
-            "INSERT INTO comunicados (docente_id, tipo_comunicado, contenido) VALUES (%s, %s, %s)",
-            (session['user_id'], tipo_comunicado, contenido)
-        )
-        comunicado_id = cur.lastrowid  # ID comunicado recién creado
-
-        cur.execute(
-            "INSERT INTO comunicados_destinatarios (comunicado_id, usuario_id) VALUES (%s, %s)",
-            (comunicado_id, destinatario)
-        )
-        mysql.connection.commit()
-        cur.close()
-
-        return redirect(url_for('lista_comunicados', mensaje='Comunicado creado con éxito.'))
-
-    # Alumnos y padres para la selección
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT id, nombre_completo FROM usuarios WHERE rol = 'alumno'")
-    alumnos = cur.fetchall()
-     
-    cur.execute("""
-        SELECT p.id, CONCAT(p.nombre_completo, ' (Hijo: ', h.nombre_completo, ')') AS nombre_con_hijo
-        FROM usuarios p
-        JOIN relacion_padre_hijo rph ON p.id = rph.padre_id
-        JOIN usuarios h ON rph.hijo_id = h.id
-        WHERE p.rol = 'padre'
-    """)
-    padres = cur.fetchall()
-    cur.close()
-
-    return render_template('crear_comunicado_personalizado.html', alumnos=alumnos, padres=padres)
-
-
-# PERFIL ALUMNO
 
 ### ESTADOS DE TAREAS
 @app.route('/tareas_alumno', methods=['GET', 'POST'])
@@ -423,6 +292,170 @@ def tareas_alumno():
     return render_template('tareas_alumno.html', tareas=tareas, nombre_completo=session['nombre_completo'])
 
 
+# COMUNICADOS
+
+# COMUNICADOS GENERALES
+@app.route('/crear_comunicado', methods=['GET', 'POST'])
+def crear_comunicado():
+    if 'user_id' not in session or session.get('role') != 'docente':
+        return redirect(url_for('index'))  
+
+    mensaje = None  # variable que almacena mensaje de éxito
+
+    if request.method == 'POST':  
+        contenido = request.form['contenido']  
+
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO comunicados (docente_id, tipo_comunicado, contenido) VALUES (%s, %s, %s)",
+            (session['user_id'], 'general', contenido)
+        )
+        mysql.connection.commit()  
+        cur.close()
+
+        mensaje = "Comunicado general creado exitosamente."
+        
+        return redirect(url_for('lista_comunicados', mensaje=mensaje))
+
+    return render_template('crear_comunicado.html', mensaje=mensaje)  
+
+
+
+
+# COMUNICADOS PERSONALIZADOS
+@app.route('/crear_comunicado_personalizado', methods=['GET', 'POST'])
+def crear_comunicado_personalizado():
+    if 'user_id' not in session or session.get('role') != 'docente':
+        return redirect(url_for('index'))  
+     
+    if request.method == 'POST':  
+        destinatario = request.form['destinatario']
+        contenido = request.form['contenido']
+        tipo_comunicado = request.form['personalizado']
+
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO comunicados (docente_id, tipo_comunicado, contenido) VALUES (%s, %s, %s)",
+            (session['user_id'], tipo_comunicado, contenido)
+        )
+        comunicado_id = cur.lastrowid  # ID comunicado recién creado
+
+        cur.execute(
+            "INSERT INTO comunicados_destinatarios (comunicado_id, usuario_id) VALUES (%s, %s)",
+            (comunicado_id, destinatario)
+        )
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('lista_comunicados', mensaje='Comunicado creado con éxito.'))
+
+    # Alumnos y padres para la selección
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id, nombre_completo FROM usuarios WHERE rol = 'alumno'")
+    alumnos = cur.fetchall()
+     
+    cur.execute("""
+        SELECT p.id, CONCAT(p.nombre_completo, ' (Hijo: ', h.nombre_completo, ')') AS nombre_con_hijo
+        FROM usuarios p
+        JOIN relacion_padre_hijo rph ON p.id = rph.padre_id
+        JOIN usuarios h ON rph.hijo_id = h.id
+        WHERE p.rol = 'padre'
+    """)
+    padres = cur.fetchall()
+    cur.close()
+
+    return render_template('crear_comunicado_personalizado.html', alumnos=alumnos, padres=padres)
+
+
+# LISTA COMUNICADOS 
+@app.route('/comunicados')
+def lista_comunicados():
+    if 'user_id' not in session:
+        return redirect(url_for('index'))
+
+    mensaje = request.args.get('mensaje')
+    user_id = session['user_id']
+    role = session.get('role')
+
+    cur = mysql.connection.cursor()
+
+    if role == 'docente':
+        cur.execute("""
+SELECT c.id, c.contenido, c.fecha_envio, c.tipo_comunicado,
+            (SELECT JSON_ARRAYAGG(JSON_OBJECT('respuesta', r.respuesta, 'remitente', u.nombre_completo, 'fecha', r.fecha_respuesta))
+            FROM respuestas_comunicados r
+            JOIN usuarios u ON r.remitente_id = u.id
+            WHERE r.comunicado_id = c.id) AS respuestas,
+            (SELECT GROUP_CONCAT(u.nombre_completo SEPARATOR ', ') 
+            FROM comunicados_destinatarios cd 
+            JOIN usuarios u ON cd.usuario_id = u.id 
+            WHERE cd.comunicado_id = c.id) AS destinatarios
+        FROM comunicados c
+        LEFT JOIN comunicados_destinatarios cd ON c.id = cd.comunicado_id
+        WHERE c.docente_id = %s OR c.tipo_comunicado = 'general' OR cd.usuario_id = %s
+        GROUP BY c.id
+        ORDER BY c.fecha_envio DESC
+        """, (user_id, user_id))
+    else:  # Rol alumno u otro
+        cur.execute("""
+        SELECT c.id, c.contenido, c.fecha_envio, c.tipo_comunicado, 
+               MAX(cd.leido) AS leido,  -- Obtenemos el estado 'leído' para el comunicado
+               (SELECT JSON_ARRAYAGG(JSON_OBJECT('respuesta', r.respuesta, 'remitente', u.nombre_completo, 'fecha', r.fecha_respuesta))
+                FROM respuestas_comunicados r
+                JOIN usuarios u ON r.remitente_id = u.id
+                WHERE r.comunicado_id = c.id) AS respuestas,
+               (SELECT GROUP_CONCAT(u.nombre_completo SEPARATOR ', ') 
+                FROM comunicados_destinatarios cd 
+                JOIN usuarios u ON cd.usuario_id = u.id 
+                WHERE cd.comunicado_id = c.id) AS destinatarios
+        FROM comunicados c
+        LEFT JOIN comunicados_destinatarios cd ON c.id = cd.comunicado_id
+        WHERE c.tipo_comunicado = 'general' OR (c.tipo_comunicado = 'personalizado' AND cd.usuario_id = %s)
+        GROUP BY c.id
+        ORDER BY c.fecha_envio DESC
+        """, (user_id,))
+
+
+    comunicados = cur.fetchall()
+    cur.close()
+
+    comunicados_format = [
+        (row[0], row[1], row[2], row[3], row[4], eval(row[5]) if row[5] else None, row[6])
+        for row in comunicados
+    ]
+
+    # print(comunicados)
+
+
+    return render_template('lista_comunicados.html', comunicados=comunicados_format, mensaje=mensaje)
+
+
+# COMUNICADOS LEIDOS
+@app.route('/marcar_leido/<int:comunicado_id>', methods=['POST'])
+def marcar_como_leido(comunicado_id):
+    if 'user_id' not in session:
+        return redirect(url_for('index'))
+    
+    user_id = session['user_id']
+
+    print(f"Comunicado ID: {comunicado_id}, Usuario ID: {user_id}") 
+
+    # Actualizamos el estado 'leído' en la tabla comunicados_destinatarios
+    cur = mysql.connection.cursor()
+    cur.execute("""
+        UPDATE comunicados_destinatarios
+        SET leido = 1
+        WHERE comunicado_id = %s AND usuario_id = %s
+    """, (comunicado_id, user_id))
+    
+    mysql.connection.commit()
+    cur.close()
+
+    print("Comunicado marcado como leído.") 
+
+    return redirect(url_for('lista_comunicados'))
+
+
 ### RESPONDER COMUNICADOS
 @app.route('/responder_comunicado/<int:comunicado_id>', methods=['POST'])
 def responder_comunicado(comunicado_id):
@@ -442,8 +475,6 @@ def responder_comunicado(comunicado_id):
     cur.close()
 
     return redirect(url_for('lista_comunicados', mensaje='Respuesta enviada con éxito.'))
-
-
 
 
 if __name__ == '__main__':
